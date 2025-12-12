82ea7c5997ea7b175e4f1e4e8df3cfc6ef33e811
Twist1978 <stefan.doepp@arcor.de>
2025-12-12 16:05:18 +0100

    composer update

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', [
+        'locale' => str_replace('_', '-', app()->getLocale()),
+        'config' => config('app.name', 'Laravel'),
+        'vorname' => 'Stefan',
+    ]);
 });
 
 Route::get('/health', function () {
